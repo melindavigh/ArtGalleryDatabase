0ec0c491339bbe998db2cbdec72ab143697c0bcb
melindavigh <129567008+melindavigh@users.noreply.github.com>
2023-04-27 19:10:00 -0500

    Update __init__.py

--- WebArtGallery/__init__.py
+++ WebArtGallery/__init__.py
@@ -1,3 +1,6 @@
+# to create python virtual enviroment run in terminal python -m venv venv
+# to activate run in terminal source venv/bin/activate
+
 from flask import Flask, render_template
 import json
 app = Flask(__name__)
