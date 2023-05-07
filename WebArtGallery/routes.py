caf14e57d35aefc759396a6339e181ba5410ab8c
Melinda Vigh <melindavigh8@gmail.com>
2023-05-07 19:40:10 -0400

    final changes

--- WebArtGallery/routes.py
+++ WebArtGallery/routes.py
@@ -13,26 +13,156 @@ def connect():
     engine = create_engine(url)
     return engine
 
+def display_function(queery):
+    engine = connect()
+    # print(queery)
+    with engine.connect() as con:
+        rs = con.execute(text(queery))
+        return_sting = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Table</title>
+            <style>
+               table {
+                    margin: 0 auto;
+                    font-size: large;
+                    border: 1px solid white;
+                }
+        
+                h1 {
+                    text-align: center;
+                    color: white;
+                    font-size: xx-large;
+                    font-family: 'Gill Sans', 'Gill Sans MT',
+                    ' Calibri', 'Trebuchet MS', 'sans-serif';
+                }
+        
+                td {
+                    text-align: center;
+                    color: white;
+                    border: 1px solid white;
+                }
+        
+                th{
+                    text-align: center;
+                    color: white;
+                    font-size: xx-large;
+                    font-family: 'Gill Sans', 'Gill Sans MT',
+                    ' Calibri', 'Trebuchet MS', 'sans-serif';
+                },
+                td {
+                    font-weight: bold;
+                    border: 1px solid white;
+                    padding: 10px;
+                    text-align: center;
+                }
+        
+                td {
+                    font-weight: lighter;
+                }
+                
+                body {
+                    background-image: url("static/img/bluesplash.jpg");
+                }
+
+            </style>
+        
+        </head>
+        <body> 
+        <table><tr>"""
+
+        for col in rs.keys():
+            return_sting += "<th>" + str(col) + "</th>"
+        return_sting += "</tr>"
+
+        for row in rs:
+            return_sting += "<tr>"
+            for x in row:
+                return_sting += "<td>" + str(x) + "</td>"
+            return_sting += "</tr>"
+                    # """"<table>
+                    #     <tr>
+                    #         <th></th>
+                    #     </tr>
+                    #     <tr>
+                    #         <td></td>
+                    #     </tr>
+                    # </table>"""
+            print(row)
+        return_sting += "</table>" + "</body></html>"
+
+    return return_sting
 @app.route("/", methods = ['GET','POST'])
 def index():
-    engine= connect()
     if request.method == 'POST':
         query_data = request.form
         # first queery
         if query_data['onechoice'] == 'queery 1':
             print(query_data['mediumList'])
-            with engine.connect() as con:
-                rs = con.execute(text(f"""SELECT DISTINCT   A.artistID, A.artistName
+            return display_function(f"""SELECT DISTINCT   A.artistID, A.artistName
                            FROM     Artist AS A, Artwork AS R
-                           WHERE    A.artistID=R.artistID AND R.medium='{query_data['mediumList']}'"""))
-                for row in rs:
-                    print(row)
-            medium = request.form.get('mediumList')
-        # secind queery goes here
-        print(request.values)
-        # elif query == 'queery 2':
-        #     print('blahhh......')
+                           WHERE    A.artistID=R.artistID AND R.medium='{query_data['mediumList']}'""")
+
+        # second queery goes here
+        elif query_data['onechoice'] == 'queery 2':
+            #print(query_data['priceRanges'])
+            return display_function(f"""SELECT   C.customerName, S.transactionPrice
+                 FROM   Customer AS C, SellTo AS S
+                 WHERE  C.customerID = S.customerID AND {query_data['priceRanges']}""")
+
+        # third queery goes here
+        elif query_data['onechoice'] == 'queery 3':
+            return display_function(f"""SELECT  R.medium,  COUNT(S.transactionQuantity) AS TotTrans
+                            FROM   Artwork AS R, Exhibit AS E, SellTo AS S, Customer AS C
+                            WHERE  R.exhibitID = E.exhibitID AND E.exhibitID =S.exhibitID AND S.customerID = C.customerID AND C.customerID=S.customerID AND S.transactionPrice >= 20000000
+                            GROUP BY  R.medium""")
+
+        # forth queery goes here
+        elif query_data['onechoice'] == 'queery 4':
+            print(query_data['genraRange'])
+            return display_function(f"""SELECT  artistName, artistID
+                        FROM Artist 
+                        WHERE  artistID   NOT IN  (SELECT  artistID
+                                   FROM   Artwork
+                                   WHERE  genre = '{query_data['genraRange']}')""")
+
+        # fifth queery goes here
+        elif query_data['onechoice'] == 'queery 5':
+            return display_function(f"""SELECT  artTitle, COUNT(DISTINCT exhibitID) AS ExhibitCount
+                FROM   Artwork
+                GROUP BY  artTitle""")
         # print(query, medium)
+
+        # sixth queery goes here
+        elif query_data['onechoice'] == 'queery 6':
+            return display_function(f"""SELECT    E.exhibitID, sum(T.transactionPrice)AS TotalRev
+                FROM Exhibit AS E, SellTo AS S, Customer AS C, Transactions AS T
+                WHERE 	E.exhibitID = S.exhibitID AND S.customerID = C.CustomerID AND C.customerID = T.customerID
+                GROUP BY    E.exhibitID""")
+
+        # seventh queery goes here
+        elif query_data['onechoice'] == 'queery 7':
+            return display_function(f"""SELECT E.address, R.artTitle 
+                FROM Exhibit AS E, Artwork as R    
+                WHERE E.exhibitID = R.exhibitID 
+                ORDER BY E.address""")
+
+        # eight queery goes here
+        elif query_data['onechoice'] == 'queery 8':
+            return display_function(f"""SELECT A.artistName, COUNT(A.artistID) AS Ct
+                FROM  Artist AS A, Exhibit AS E, Registered AS R
+                WHERE A.artistID = R.artistID AND R.exhibitID = E.exhibitID
+                GROUP BY  A.artistName
+                HAVING COUNT(A.artistID) >= 3""")
+
+        # nineth queery goes here
+        elif query_data['onechoice'] == 'queery 9':
+            print(query_data['exhibitList'])
+            return display_function(f"""SELECT A.artistID, A.artistName
+                FROM Artist AS A, Exhibit AS E, Registered AS R
+                WHERE A.artistID=R.artistID AND E.exhibitID=R.exhibitID AND E.exhibitName='{query_data['exhibitList']}'""")
+
     return render_template("helloworld.html")
 
 @app.route("/sayhello/<name>")
--- main.py
+++ main.py
@@ -21,52 +21,52 @@ engine = create_engine(url)  # sql connection
 #     for row in rs:
 #         print(row)
 #
-for table in ["Artwork", "Exhibit", "Customer", "Transactions", "Registered", "SellTo", "Artist"]:
-    df = pd.read_excel('ArtGalleryDataBase copy.xlsx', sheet_name=table, skiprows=1) # reads from excel file, skips forst row cause we dont want it
-    df.to_sql(table, engine, if_exists='replace', index=False) # this writes the tables to sql. It'll also update any changes we make to the excel file
-    print(df)
-
-
-
-print("1. Display name and ID of Artist who use “oil paint” as a medium for their artwork")
-with engine.connect() as con:
-     rs = con.execute(text("""SELECT DISTINCT   A.artistID, A.artistName
-                FROM     Artist AS A, Artwork AS R
-                WHERE    A.artistID=R.artistID AND R.medium='oil paint'"""))
-     for row in rs:
-         print(row)
-print()
+# for table in ["Artwork", "Exhibit", "Customer", "Transactions", "Registered", "SellTo", "Artist"]:
+#     df = pd.read_excel('ArtGalleryDataBase copy.xlsx', sheet_name=table, skiprows=1) # reads from excel file, skips forst row cause we dont want it
+#     df.to_sql(table, engine, if_exists='replace', index=False) # this writes the tables to sql. It'll also update any changes we make to the excel file
+#     print(df)
+# #
 #
+#
+# print("1. Display name and ID of Artist who use “oil paint” as a medium for their artwork")
+# with engine.connect() as con:
+#      rs = con.execute(text("""SELECT DISTINCT   A.artistID, A.artistName
+#                 FROM     Artist AS A, Artwork AS R
+#                 WHERE    A.artistID=R.artistID AND R.medium='oil paint'"""))
+#      for row in rs:
+#          print(row)
+# print()
+# #
 # print("2. Display name of Customers, Transaction ID, and transactionPrice who purchase artwork between the range of $50000000 - $100000000")
 # with engine.connect() as con:
 #     rs2 = con.execute(text("""SELECT   C.customerName, T.transactionID, T.transactionPrice
-#                 FROM   Customer AS C, Transactions AS T
-#                 WHERE  C.customerID = T.customerID AND T.transactionPrice >=50000000 AND T.transactionPrice<=100000000"""))
+#                  FROM   Customer AS C, Transactions AS T
+#                  WHERE  C.customerID = T.customerID AND T.transactionPrice >=50000000 AND T.transactionPrice<=100000000"""))
 #     for row in rs2:
 #         print(row)
 # print()
-#
-print("3. Count the number of transactions with price higher than $20000000 per artwork medium")
-with engine.connect() as con:
-    rs3 = con.execute(text("""SELECT  R.medium,  COUNT(T.transactionID) AS TotTrans
-            FROM   Artwork AS R, Exhibit AS E, SellTo AS S, Customer AS C, Transactions AS T
-            WHERE  R.exhibitID = E.exhibitID AND E.exhibitID =S.exhibitID AND S.customerID = C.customerID AND C.customerID=T.customerID AND T.transactionPrice >= 20000000
-            GROUP BY  R.medium"""))
-    for row in rs3:
-        print(row)
-print()
-
-print("4. Display name and ID of artists who doesn’t create artwork in the ‘realism’ genre ")
-with engine.connect() as con:
-    rs4 = con.execute(text("""SELECT  artistName, artistID
-        FROM Artist AS A
-        WHERE  artistID   NOT IN  (SELECT  artistID
-                                   FROM   Artwork
-                                   WHERE  genre = 'Realism')"""))
-    for row in rs4:
-        print(row)
-print()
 # #
+# print("3. Count the number of transactions with price higher than $20000000 per artwork medium")
+# with engine.connect() as con:
+#     rs3 = con.execute(text("""SELECT  R.medium,  COUNT(T.transactionID) AS TotTrans
+#             FROM   Artwork AS R, Exhibit AS E, SellTo AS S, Customer AS C, Transactions AS T
+#             WHERE  R.exhibitID = E.exhibitID AND E.exhibitID =S.exhibitID AND S.customerID = C.customerID AND C.customerID=T.customerID AND T.transactionPrice >= 20000000
+#             GROUP BY  R.medium"""))
+#     for row in rs3:
+#         print(row)
+# print()
+#
+# print("4. Display name and ID of artists who doesn’t create artwork in the ‘realism’ genre ")
+# with engine.connect() as con:
+#     rs4 = con.execute(text("""SELECT  artistName, artistID
+#         FROM Artist
+#         WHERE  artistID   NOT IN  (SELECT  artistID
+#                                    FROM   Artwork
+#                                    WHERE  genre = 'Realism')"""))
+#     for row in rs4:
+#         print(row)
+# print()
+# # #
 # print("5. Display the number of exhibits each artwork has been to sorted by their titles")
 # with engine.connect() as con:
 #     rs6 = con.execute(text("""SELECT  artTitle, COUNT(DISTINCT exhibitID) AS ExhibitCount
@@ -75,7 +75,7 @@ print()
 #     for row in rs6:
 #         print(row)
 # print()
-#
+# #
 # print("6. Display the total revenue each exhibit has generated per exhibitID")
 # with engine.connect() as con:
 #     rs6 = con.execute(text("""SELECT    E.exhibitID, sum(T.transactionPrice)AS TotalRev
@@ -84,44 +84,46 @@ print()
 #                 GROUP BY    E.exhibitID"""))
 #     for row in rs6:
 #         print(row)
-# print()
+# # print()
+# #
+# # print("7. Display the highest transaction price per medium ")
+# # with engine.connect() as con:
+# #     rs7 = con.execute(text(""" SELECT  R.medium, max(T.transactionPrice) AS MaxPrice
+# #          FROM    Artwork AS R, Exhibit as E, Transactions AS T, SellTo AS S, Customer AS C
+# #          WHERE   R.exhibitID =E.exhibitID AND E.exhibitID = S.exhibitID AND S.customerID =C.customerID and C.customerID=T.customerID
+# #          GROUP BY    R.medium"""))
+# #     for row in rs7:
+# #         print(row)
+# # print()
 #
-# print("7. Display the highest transaction price per medium ")
+#
+# print("7. Display the location of the exhibit and artTitle for each artwork sorted by location ")
 # with engine.connect() as con:
-#     rs7 = con.execute(text(""" SELECT  R.medium, max(T.transactionPrice) AS MaxPrice
-#          FROM    Artwork AS R, Exhibit as E, Transactions AS T, SellTo AS S, Customer AS C
-#          WHERE   R.exhibitID =E.exhibitID AND E.exhibitID = S.exhibitID AND S.customerID =C.customerID and C.customerID=T.customerID
-#          GROUP BY    R.medium"""))
+#     rs7 = con.execute(text("""SELECT E.address, R.artTitle
+#         FROM Exhibit AS E, Artwork as R
+#         WHERE E.exhibitID = R.exhibitID
+#         ORDER BY E.address"""))
 #     for row in rs7:
 #         print(row)
 # print()
 
-
-print("7. Display the highest transaction price per medium ")
+#
+print("8. Display the names of the artists who went to more than 3 exhibits")
 with engine.connect() as con:
-    rs7 = con.execute(text(""" SELECT  R.medium, max(S.transactionPrice) AS MaxPrice
-         FROM    Artwork AS R, SellTo as S, Customer as C, Exhibit AS E
-         WHERE   C.customerID = S.customerID AND E.exhibitID = S.exhibitID AND E.exhibitID = R.exhibitID
-         GROUP BY    R.medium"""))
-    for row in rs7:
+    rs8 = con.execute(text("""SELECT A.artistName, COUNT(A.artistID) AS Ct
+                FROM  Artist AS A, Exhibit AS E, Registered AS R
+                WHERE A.artistID = R.artistID AND R.exhibitID = E.exhibitID
+                GROUP BY  A.artistName
+                HAVING COUNT(A.artistID) >= 3"""))
+for row in rs8:
         print(row)
 print()
 #
-# print("8. Display the names and count of all the artists who have been to the most exhibits sorted by the artist name. ")
-# # with engine.connect() as con:
-# # rs8 = con.execute(text("""SELECT A.artistName, COUNT(E.exhibitID) AS MostArt
-# # 	    FROM  Artist AS A, Exhibit AS E, Registered AS G
-# # 	    WHERE A.artistID = G.artistID AND G.exhibitID = E.exhibitID;
-# # 	    GROUP BY A.artistName"""))
-# # for row in rs8:
-# #         print(row)
-# # print()
-
-print("9. Display id and names of artists who have artworks registered in exhibit “Beautiful Passion” or choose your own exhibit")
-with engine.connect() as con:
-    rs7 = con.execute(text(""" SELECT A.artistID, A.artistName
-                FROM Artist AS A, Exhibit AS E, Registered AS R
-                WHERE A.artistID=R.artistID AND E.exhibitID=R.exhibitID AND E.exhibitName='Sterling Views'"""))
-    for row in rs7:
-        print(row)
-print()
+# print("9. Display id and names of artists who have artworks registered in exhibit “Beautiful Passion” or choose your own exhibit")
+# with engine.connect() as con:
+#     rs7 = con.execute(text(""" SELECT A.artistID, A.artistName
+#                 FROM Artist AS A, Exhibit AS E, Registered AS R
+#                 WHERE A.artistID=R.artistID AND E.exhibitID=R.exhibitID AND E.exhibitName='Sterling Views'"""))
+#     for row in rs7:
+#         print(row)
+# print()
